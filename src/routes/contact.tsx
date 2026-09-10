8eb0f8585c656b9af9c764a857d93e80bb0f5f47
PPTQ Nurul Huda <muhammadmardliyata@gmail.com>
2026-09-10 10:29:30 +0700

    Update website

--- src/routes/contact.tsx
+++ src/routes/contact.tsx
@@ -31,12 +31,13 @@ export const Route = createFileRoute("/contact")({
 function ContactPage() {
   const details = [
     { icon: MapPin, label: "Alamat", value: school.address },
-    { icon: Phone, label: "Telepon", value: school.phone, href: `tel:${school.phone.replace(/\s/g, "")}` },
+    // { icon: Phone, label: "Telepon", value: school.phone, href: `tel:${school.phone.replace(/\s/g, "")}` },
+    { icon: Phone, label: "Telepon", value: '082338832018', href: `tel:082338832018` },
     {
       icon: MessageCircle,
       label: "WhatsApp",
-      value: school.whatsappDisplay,
-      href: `https://wa.me/${school.whatsapp}`,
+      value: '082338832018',
+      href: `https://wa.me/082338832018`,
     },
     { icon: Mail, label: "Email", value: school.email, href: `mailto:${school.email}` },
     { icon: Clock, label: "Jam Layanan", value: school.hours },
